b42071f8c234ff581887c8f4d46d4c3641cc59eb
rahulkrishnan98 <mrahul.krishnan@gmail.com>
2020-04-10 20:35:57 +0530

    life-saver Gopala

--- GeekTrustFamilyTreeProblem-Python/family.py
+++ GeekTrustFamilyTreeProblem-Python/family.py
@@ -22,39 +22,39 @@ class Family:
     '''
 
     def add_child_in_family(self, mother, child):
-        mother = Family.search_member(self, mother.name)
+        mother = self.search_member(mother.name)
         mother.add_children(child)
         self.family_members.append(child)
     
    
     def add_spouse_in_family(self, member, spouse):
-        member = Family.search_member(self,member.name)
+        member = self.search_member(member.name)
         member.add_spouse(spouse)
         spouse.add_spouse(member)
         self.family_members.append(spouse)
 
     
     def get_relationship(self, member, relation):
-        person = Family.search_member(self, member.name)
+        person = self.search_member(member.name)
 
         if(relation == constants.SIBLINGS):
-            result = Family.in_order(self,[sibling.name for sibling in person.get_brother()+ person.get_sister()])
+            result = self.in_order([sibling.name for sibling in person.get_brother()+ person.get_sister()])
         elif(relation == constants.PATERNAL_UNCLE):
-            result = Family.in_order(self, [per.name for per in person.get_paternal_uncle()])
+            result = self.in_order([per.name for per in person.get_paternal_uncle()])
         elif(relation == constants.PATERNAL_AUNT):
-            result = Family.in_order(self, [per.name for per in person.get_paternal_aunt()])
+            result = self.in_order([per.name for per in person.get_paternal_aunt()])
         elif(relation == constants.MATERNAL_UNCLE):
-            result = Family.in_order(self, [per.name for per in person.get_maternal_uncle()])
+            result = self.in_order([per.name for per in person.get_maternal_uncle()])
         elif(relation == constants.MATERNAL_AUNT):
-            result = Family.in_order(self, [per.name for per in person.get_maternal_aunt()])
+            result = self.in_order([per.name for per in person.get_maternal_aunt()])
         elif(relation == constants.COMMAND_SON):
-            result = Family.in_order(self, [per.name for per in person.sons])
+            result = self.in_order([per.name for per in person.sons])
         elif(relation == constants.COMMAND_DAUGHTER):
-            result = Family.in_order(self, [per.name for per in person.daughters])
+            result = self.in_order([per.name for per in person.daughters])
         elif(relation == constants.SISTER_IN_LAW):
-            result = Family.in_order(self, [inlaw.name for inlaw in person.get_sister_in_law()])
+            result = self.in_order([inlaw.name for inlaw in person.get_sister_in_law()])
         elif(relation == constants.BROTHER_IN_LAW):
-            result = Family.in_order(self, [inlaw.name for inlaw in person.get_brother_in_law()])
+            result = self.in_order([inlaw.name for inlaw in person.get_brother_in_law()])
         else:
             raise custom_exceptions.CommandNotFound(constants.COMMAND_ERROR)
         if(len(result) == 0):
@@ -65,7 +65,7 @@ class Family:
 
     def in_order(self, arr):
         result = []
-        arr = [Family.search_member(self, arr[i]) for i in range(len(arr))]
+        arr = [self.search_member(arr[i]) for i in range(len(arr))]
         members = self.family_members
         for member in members:
             if(member in arr):
